c41cc7b11fc03fd218c5fcdcad354fa0d026dfff
Sumitkumar1109 <139613858+Sumitkumar1109@users.noreply.github.com>
2025-01-23 08:40:35 +0530

    feat/initial folder structure set up

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/connectDB')
+const authRoutes = require('./routes/auth.route');
+const cors = require('cors');
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+
+
+const PORT = process.env.PORT || 8000;
+
+connectDB().then(() => {
+    app.listen(PORT, (err) => {
+        if (err) {
+            console.error(err);
+        }
+        console.log(`Server is running successfully on port: ${PORT}`);
+    });
+}).catch((err) => {
+    console.error(err);
+})
