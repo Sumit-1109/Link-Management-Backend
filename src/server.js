119ee35cfd5e480fb928282ccfd0d592b8aa45a7
Sumitkumar1109 <sumitkumar2000sep@gmail.com>
2025-01-30 15:18:13 +0530

    feat\fixed cors issues\origin

--- src/server.js
+++ src/server.js
@@ -14,8 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('*', cors({
-    // origin: 'https://link-management-frontend.vercel.app',
-    origin: 'http://localhost:5173',
+    origin: 'https://link-management-frontend.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
