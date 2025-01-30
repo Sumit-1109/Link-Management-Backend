841cfef16f71e15912c9e904bb8277d513e4e1ad
Sumitkumar1109 <sumitkumar2000sep@gmail.com>
2025-01-31 05:22:40 +0530

    feat\fixed cors issue

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('*', cors({
+app.use(cors({
     origin: 'https://link-management-frontend.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
